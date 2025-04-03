5234e90e4b3f8250848ba92b3903eea43d1a3c6c
poorniv-89 <poorniv.89@gmail.com>
2025-04-03 13:22:42 -0600

    Part 2 done

--- script.js
+++ script.js
@@ -7,16 +7,39 @@ const adventurer = {
     companion: {
         name: "Leo",
         companion: {
-            name:"Frank",
-            type:"Flea",
+            name: "Frank",
+            type: "Flea",
             belongings: ["hat", "sunglasses"]
         },
         type: "Cat"
     },
-    roll (mod = 0) {
+    roll(mod = 0) {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
     }
-    }
+}
 
 console.log(adventurer.companion.companion.belongings[1]);
+
+//Part 2: Class Fantasy
+class Character {
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`)
+    }
+}
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.companion.roll();
+robin.companion.companion.roll();
