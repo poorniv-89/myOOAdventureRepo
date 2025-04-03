fb3f135a227cae6c28bf2fcea60b14d46d1ec1c4
poorniv-89 <poorniv.89@gmail.com>
2025-04-03 13:54:56 -0600

    part 3 is done

--- script.js
+++ script.js
@@ -33,13 +33,66 @@ class Character {
         console.log(`${this.name} rolled a ${result}.`)
     }
 }
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
+// const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-robin.companion.roll();
-robin.companion.companion.roll();
+// robin.companion.roll();
+// robin.companion.companion.roll();
+
+//Part 3: Class Features
+class Adventurer extends Character {
+    constructor(name, role, level, stamina) {
+        super(name);
+        // Adventurers have specialized roles.
+        this.role = role;
+        this.level = level;
+        this.stamina = stamina;
+        // Every adventurer starts with a bed and 50 gold coins.
+        this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout() {
+        console.log(`${this.name} is scouting ahead...`);
+        super.roll();
+    }
+    attack() {
+        if (this.stamina > 50) {
+            console.log(`${this.name} still has ${this.stamina} percent stamina to attack`);
+        }
+        else {
+            console.log(`${this.name} is too tired to attack. Stamina is low.`);
+        }
+    }
+}
+  class Companion {
+    constructor(name, type, companion) {
+        this.name = name;
+        this.type = type;
+        this.companionName = companion[0];
+        this.companionType = companion[1];
+        this.companionInventory = companion[2];
+    }
+    follow() {
+        console.log(`${this.name} the ${this.type} is following you.`);
+      }
+    
+    rest() {
+        console.log(`${this.name} the ${this.type} is resting.`);
+      }
+}
+
+const robin = new Adventurer("Robin", "warrior", 20, 78);
+robin.roll();
+robin.scout();
+robin.attack();
+
+const leo = new Companion("Leo", "Cat", ["Frank", "Flea", ["hat", "sunglasses"]])
+const pickles = new Companion("pickles", "bird", ["bleuberry", "bird", ["binoculars", "sunglasses"]])
+console.log(leo, pickles);
+leo.rest();
+pickles.follow();
